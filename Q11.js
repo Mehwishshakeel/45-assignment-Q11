bfdea6375515dfcfe415d4775f99a7ecc24f6464
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-03-06 16:08:45 +0500

    Add files via upload

---
+++ Q11.js
@@ -0,0 +1,5 @@
+"use strict";
+// Question 11:
+// use forEach function to print each name one at a time in a list
+let friendsName = ["Sana ", "Sara", "Sameen", "Sadaf", "Shaheen", "Samreen"];
+friendsName.forEach(friendname => console.log(friendname));
---
+++ Q11.ts
@@ -0,0 +1,4 @@
+// Question 11:
+// use forEach function to print each name one at a time in a list
+let friendsName = ["Sana ", "Sara","Sameen","Sadaf","Shaheen","Samreen"];
+friendsName.forEach(friendname => console.log(friendname));
